46cc4065a37622cd12f0cb0b3aada74b428bf340
Martin Lee <2548252929@qq.com>
2021-01-22 10:38:37 +0800

    init

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import sys
+import time
+from config import global_config
+from jdlogger import logger
 from jd_mask_spider_requests import Jd_Mask_Spider
 
 if __name__ == '__main__':
     a = """
-  ________   __    _ 
- |___  /\ \ / /   | |
-    / /  \ V /    | |
-   / /    > < _   | |
-  / /__  / . \ |__| |1.预约商品
- /_____|/_/ \_\____/ 2.秒杀抢购商品 
+1.预约商品
+2.秒杀抢购商品 
     """
+    print(a)
     try:
         start_tool = Jd_Mask_Spider()
-        print(a)
         choice_function = input('选择功能:')
         if choice_function == '1':
             start_tool.login()
             start_tool.make_reserve()
         elif choice_function == '2':
-            start_tool.request_seckill_url()
-            start_tool.request_seckill_checkout_page()
-            start_tool.submit_seckill_order()
+            tryTimes = 0
+            skill_count = global_config.getRaw('config', 'kill_count')
+            kill_per_time = global_config.getRaw('config', 'kill_per_time')
+            while True:
+                tryTimes = tryTimes + 1
+                logger.info("-------------------------------------->开始第{}次抢购<--------------------------------------".format(tryTimes))
+                start_tool.request_seckill_url()
+                start_tool.request_seckill_checkout_page()
+                # 可能需要的链接
+                # https://tak.jd.com/t/98DD1?_t=1611281236817
+                start_tool.submit_seckill_order()
+                time.sleep(kill_per_time)
+            logger.info("-------------------------------------->完成了{}次抢购,你运气不好没办法<--------------------------------------".format(tryTimes))
         else:
-            print('没有此功能')
+            logger.error("没有此功能")
             sys.exit(1)
     except Exception as e:
-        print("error {}".format(e))
+        logger.error("error {}".format(e))
